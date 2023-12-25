35d42b2c0d9db71cfe7684ea67a342107f18f60c
XLoganxmenX <loganxmen97@gmail.com>
2023-12-25 21:28:38 +0900

    add SimpleQueue and test [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,38 @@
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+    private int inSize = 0;
+    private int outSize = 0;
+
+
+    public T poll() {
+        if (inSize == 0 && outSize == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        if (outSize == 0) {
+            while (inSize != 0) {
+                out.push(in.pop());
+                inSize--;
+                outSize++;
+            }
+        }
+
+        T returnVal = null;
+        if (outSize != 0) {
+            returnVal = out.pop();
+            outSize--;
+        }
+
+        return returnVal;
+    }
+
+    public void push(T value) {
+        in.push(value);
+        inSize++;
+    }
+}
