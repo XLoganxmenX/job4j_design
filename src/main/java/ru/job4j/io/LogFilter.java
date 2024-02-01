0988bb497bca8570e755aae394895e72de042af8
XLoganxmenX <loganxmen97@gmail.com>
2024-02-01 20:39:31 +0900

    update LogFilter (change 'filter' method) [#252489 #477983]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,26 +1,36 @@
 package ru.job4j.io;
 
+import ru.job4j.generics.Predator;
+
 import java.io.*;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogFilter {
     private final String file;
-    private static final String MESSAGE = " 404 ";
+    private static final String MESSAGE = "404";
 
     public LogFilter(String file) {
         this.file = file;
     }
 
     public List<String> filter() {
-        List<String> list = new LinkedList<>();
+        List<String> outputList = new LinkedList<>();
         try (BufferedReader input = new BufferedReader(new FileReader(file))) {
-            list = input.lines().filter(line -> line.contains(MESSAGE)).collect(Collectors.toList());
+            for (String line : input.lines().toList()) {
+                String[] word = line.split("\\W+");
+                if (word[word.length - 2].equals(MESSAGE)) {
+                    outputList.add(line);
+                }
+            }
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return list;
+        return outputList;
     }
 
     public void saveTo(String out) {
