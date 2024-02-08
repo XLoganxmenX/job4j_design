5a35b8e38642abb0f600b89fed2fbe6bb5ec6687
XLoganxmenX <loganxmen97@gmail.com>
2024-02-08 20:50:20 +0900

    change Analysis class (refactor unavailable method, add printShutdownTime method) [#859 #479617]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,45 +6,37 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Analysis {
+
+    private boolean isSrvOnline = true;
     public void unavailable(String source, String target) {
-        List<String> lines;
-        List<String> timeList = new ArrayList<>(8);
-        boolean isSrvOnline = true;
+
 
         try (BufferedReader input = new BufferedReader(new FileReader(source));
                 PrintWriter output = new PrintWriter(
                         new BufferedWriter(
                                 new FileWriter(target)))) {
-
-            lines = input.lines().collect(Collectors.toList());
-            for (String line : lines) {
-                String[] word = line.split(" ", 2);
-                StringBuilder builder = new StringBuilder();
-
-                if (isSrvOnline && ("400".equals(word[0]) || "500".equals(word[0]))) {
-                    isSrvOnline = false;
-                    builder.append(word[1]).append(";");
-                }
-
-                if (!isSrvOnline && ("200".equals(word[0]) || "300".equals(word[0]))) {
-                    isSrvOnline = true;
-                    builder.append(word[1]).append(";");
-                }
-
-                if (!builder.isEmpty()) {
-                    timeList.add(builder.toString());
-                }
-            }
-
-            for (int i = 0; i < timeList.size(); i = i + 2) {
-                output.printf(timeList.get(i) + timeList.get(i + 1));
-                if (i + 2 < timeList.size()) {
-                    output.print(System.lineSeparator());
-                }
-            }
-
+            input.lines().forEach(line -> printShutdownTime(line, output));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void printShutdownTime(String line, PrintWriter output) {
+        String[] word = line.split(" ", 2);
+        StringBuilder builder = new StringBuilder();
+
+        if (isSrvOnline && ("400".equals(word[0]) || "500".equals(word[0]))) {
+            isSrvOnline = false;
+            builder.append(word[1]).append(";");
+        }
+
+        if (!isSrvOnline && ("200".equals(word[0]) || "300".equals(word[0]))) {
+            isSrvOnline = true;
+            builder.append(word[1]).append(";").append(System.lineSeparator());
+        }
+
+        if (!builder.isEmpty()) {
+            output.print(builder);
         }
     }
 
