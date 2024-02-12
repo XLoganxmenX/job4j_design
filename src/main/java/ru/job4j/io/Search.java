aa30e7921a2f1fd3d268e281be9b48c076f13acf
XLoganxmenX <loganxmen97@gmail.com>
2024-02-12 21:38:17 +0900

    update Search (add validate) [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -6,16 +6,38 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validate(args);
+
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1].toLowerCase())).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+
+        if (args[0].length() == 0) {
+            throw new IllegalArgumentException("Root folder is null. Enter correct path.");
+        }
+
+        Path start = Paths.get(args[0]);
+        if (!Files.exists(start)) {
+            throw new IllegalArgumentException("File/directory not exist. Enter correct path.");
+        }
+
+        if (args[1].length() == 0 || !Pattern.matches("[a-zA-Z\\d]+", args[1])) {
+            throw new IllegalArgumentException("Enter correct file format.");
+        }
     }
 }
