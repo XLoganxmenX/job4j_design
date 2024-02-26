07e5349682d29093d868526a54c4a2bde01d980a
XLoganxmenX <loganxmen97@gmail.com>
2024-02-26 20:27:13 +0900

    update EchoServer (add checkMessage method) [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,6 +5,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    
+    private static void checkMessage(ServerSocket server, String message, OutputStream output) throws IOException {
+        if (message.contains("msg=Hello")) {
+            output.write("Hello\r\n".getBytes());
+        } else if (message.contains("msg=Exit")) {
+            output.write("Close server\r\n".getBytes());
+            server.close();
+        } else {
+            output.write("What\r\n".getBytes());
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -16,10 +28,7 @@ public class EchoServer {
 
                     String firstHTTPMessage = input.readLine();
                     System.out.println(firstHTTPMessage);
-                    if (firstHTTPMessage.contains("msg=Bye")) {
-                        output.write("Close server\r\n".getBytes());
-                        server.close();
-                    }
+                    checkMessage(server, firstHTTPMessage, output);
 
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
                         System.out.println(string);
