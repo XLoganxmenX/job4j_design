28ae4c71ddde4328b57b593edc320f23f6f93bc4
XLoganxmenX <loganxmen97@gmail.com>
2024-05-16 20:07:02 +0900

    update GitExample (add constructor and push method)

--- src/main/java/ru/job4j/git/GitExample.java
+++ src/main/java/ru/job4j/git/GitExample.java
@@ -8,4 +8,13 @@ import java.util.Map;
 public class GitExample {
     List<String> gitHub = new ArrayList<>();
     Map<Integer, String> gitLab = new HashMap<>();
+    String commit;
+
+    public GitExample(String commit) {
+        this.commit = commit;
+    }
+
+    public void push() {
+        gitHub.add(commit);
+    }
 }
