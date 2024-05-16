d2c51bca0639681c1dafe09ee92f743dca04edea
XLoganxmenX <loganxmen97@gmail.com>
2024-05-16 19:59:48 +0900

    update GitExample

--- src/main/java/ru/job4j/git/GitExample.java
+++ src/main/java/ru/job4j/git/GitExample.java
@@ -1,8 +1,11 @@
 package ru.job4j.git;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GitExample {
     List<String> gitHub = new ArrayList<>();
+    Map<Integer, String> gitLab = new HashMap<>();
 }
